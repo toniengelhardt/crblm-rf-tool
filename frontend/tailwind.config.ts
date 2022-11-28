c196b79fec521af9a108008d05dcdb9e1ce8ffd9
Toni <2703233+toniengelhardt@users.noreply.github.com>
2022-11-28 21:38:21 +0000

    Clean up

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,21 +3,9 @@ import tailwindNesting from '@tailwindcss/nesting'
 import tailwindTypography from '@tailwindcss/typography'
 
 export default <Partial<Config>>{
-  theme: {
-    // extend: {
-    //   colors: {
-    //     primary: defaultTheme.colors.green
-    //   }
-    // }
-  },
   plugins: [
     tailwindNesting,
     tailwindTypography,
     require('daisyui'),
   ],
-  // daisyui: {
-  //   themes: [
-  //     'cupcake',
-  //   ],
-  // }
 }
